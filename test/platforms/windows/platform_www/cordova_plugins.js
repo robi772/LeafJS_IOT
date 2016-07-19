9c4882abc80cab66d713fc630c67a4a14d2c99a7
Robert Juhasz <robi772@gmail.com>
2016-07-19 08:29:03 +0200

    Init

---
+++ test/platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,188 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
+        "id": "org.apache.cordova.file.DirectoryEntry",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.DirectoryEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryReader.js",
+        "id": "org.apache.cordova.file.DirectoryReader",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.DirectoryReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Entry.js",
+        "id": "org.apache.cordova.file.Entry",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.Entry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/File.js",
+        "id": "org.apache.cordova.file.File",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.File"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileEntry.js",
+        "id": "org.apache.cordova.file.FileEntry",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileError.js",
+        "id": "org.apache.cordova.file.FileError",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileReader.js",
+        "id": "org.apache.cordova.file.FileReader",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileSystem.js",
+        "id": "org.apache.cordova.file.FileSystem",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadOptions.js",
+        "id": "org.apache.cordova.file.FileUploadOptions",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileUploadOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadResult.js",
+        "id": "org.apache.cordova.file.FileUploadResult",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileUploadResult"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileWriter.js",
+        "id": "org.apache.cordova.file.FileWriter",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.FileWriter"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Flags.js",
+        "id": "org.apache.cordova.file.Flags",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.Flags"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/LocalFileSystem.js",
+        "id": "org.apache.cordova.file.LocalFileSystem",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.LocalFileSystem"
+        ],
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Metadata.js",
+        "id": "org.apache.cordova.file.Metadata",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.Metadata"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/ProgressEvent.js",
+        "id": "org.apache.cordova.file.ProgressEvent",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.ProgressEvent"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/fileSystems.js",
+        "id": "org.apache.cordova.file.fileSystems",
+        "pluginId": "org.apache.cordova.file"
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/requestFileSystem.js",
+        "id": "org.apache.cordova.file.requestFileSystem",
+        "pluginId": "org.apache.cordova.file",
+        "clobbers": [
+            "window.requestFileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/resolveLocalFileSystemURI.js",
+        "id": "org.apache.cordova.file.resolveLocalFileSystemURI",
+        "pluginId": "org.apache.cordova.file",
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/src/windows/FileProxy.js",
+        "id": "org.apache.cordova.file.FileProxy",
+        "pluginId": "org.apache.cordova.file",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file-transfer/www/FileTransferError.js",
+        "id": "org.apache.cordova.file-transfer.FileTransferError",
+        "pluginId": "org.apache.cordova.file-transfer",
+        "clobbers": [
+            "window.FileTransferError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file-transfer/www/FileTransfer.js",
+        "id": "org.apache.cordova.file-transfer.FileTransfer",
+        "pluginId": "org.apache.cordova.file-transfer",
+        "clobbers": [
+            "window.FileTransfer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file-transfer/src/windows/FileTransferProxy.js",
+        "id": "org.apache.cordova.file-transfer.FileTransferProxy",
+        "pluginId": "org.apache.cordova.file-transfer",
+        "clobbers": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.console": "0.2.13",
+    "org.apache.cordova.file": "1.3.3",
+    "org.apache.cordova.file-transfer": "0.5.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ test/www/index.js
@@ -0,0 +1,128 @@
+var LAT  = 47.891;
+var LNG  = 20.382;
+var ZOOM = 13;
+
+var CACHE_ZOOM_MIN = 13, CACHE_ZOOM_MAX = 19;
+
+var MAP, BASE;
+
+function startMap() {
+    MAP = L.map('map', {
+        minZoom:CACHE_ZOOM_MIN,
+        maxZoom:CACHE_ZOOM_MAX
+    });
+
+    try {
+        BASE = L.tileLayerCordova('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            // these options are perfectly ordinary L.TileLayer options
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright/">OpenStreetMap contributors</a>',
+            // these are specific to L.TileLayer.Cordova and mostly specify where to store the tiles on disk
+            folder: 'LTileLayerCordovaExample',
+            name:   'example',
+            debug:   true
+        }, function() {
+			updateStatus();
+		}).addTo(MAP);
+    } catch (e) {
+        alert(e);
+    }
+
+    MAP.setView([LAT,LNG],ZOOM);
+    MAP.on('moveend', function () {
+		updateStatus();
+    }).fire('moveend');
+}
+
+function updateStatus() {
+	document.getElementById('status').innerHTML =  MAP.getCenter().lat.toFixed(5) + ' x ' + MAP.getCenter().lng.toFixed(5) + ' @ ' + MAP.getZoom() + (BASE.isOnline() ? ' (ONLINE)' : BASE.isOffline() ? ' (OFFLINE)' : '');
+}
+
+function startButtons() {
+    document.getElementById('test_cache_pyramid').addEventListener('click', testCachingPyramid);
+    document.getElementById('test_cache_bounds').addEventListener('click', testCachingBounds);
+    document.getElementById('test_offline').addEventListener('click', testOffline);
+    document.getElementById('test_online').addEventListener('click', testOnline);
+    document.getElementById('test_usage').addEventListener('click', testUsage);
+    document.getElementById('test_browse_cache').addEventListener('click', testBrowseCache);
+    document.getElementById('test_empty').addEventListener('click', testEmpty);
+}
+
+function testCachingPyramid() {
+	testCaching('pyramid');
+}
+
+function testCachingBounds() {
+	testCaching('bounds');
+}
+
+function testCaching(which) {
+    var lat       = MAP.getCenter().lat;
+    var lng       = MAP.getCenter().lng;
+    var zmin      = MAP.getZoom();
+    var zmax      = CACHE_ZOOM_MAX;
+    var tile_list = (which == 'pyramid' ? BASE.calculateXYZListFromPyramid(lat, lng, zmin, zmax) : BASE.calculateXYZListFromBounds(MAP.getBounds(), zmin, zmax));
+    var message   = "Preparing to cache tiles.\n" + "Zoom level " + zmin + " through " + zmax + "\n" + tile_list.length + " tiles total." + "\nClick OK to proceed.";
+    var ok        = confirm(message);
+    if (! ok) return false;
+
+    var status_block = document.getElementById('status');
+
+    BASE.downloadXYZList(
+        // 1st param: a list of XYZ objects indicating tiles to download
+        tile_list,
+        // 2nd param: overwrite existing tiles on disk? if no then a tile already on disk will be kept, which can be a big time saver
+        false,
+        // 3rd param: progress callback
+        // receives the number of tiles downloaded and the number of tiles total; caller can calculate a percentage, update progress bar, etc.
+        function (done,total) {
+            var percent = Math.round(100 * done / total);
+            status_block.innerHTML = done  + " / " + total + " = " + percent + "%";
+        },
+        // 4th param: complete callback
+        // no parameters are given, but we know we're done!
+        function () {
+            // for this demo, on success we use another L.TileLayer.Cordova feature and show the disk usage
+            testUsage();
+        },
+        // 5th param: error callback
+        // parameter is the error message string
+        function (error) {
+            alert("Failed\nError code: " + error.code);
+        }
+    );
+}
+
+function testOffline() {
+    BASE.goOffline();
+	updateStatus();
+}
+
+function testOnline() {
+    BASE.goOnline();
+	updateStatus();
+}
+
+function testUsage() {
+    var status_block = document.getElementById('status');
+    BASE.getDiskUsage(function (filecount,bytes) {
+        var kilobytes = Math.round( bytes / 1024 );
+        status_block.innerHTML = "Cache status" + "<br/>" + filecount + " files" + "<br/>" + kilobytes + " kB";
+    });
+}
+
+function testBrowseCache() {
+	BASE.getCacheContents(function(cache) {
+		console.log(cache);
+		alert("Look in console for cache contents");
+	});
+}
+
+function testEmpty() {
+    BASE.emptyCache(function (oks,fails) {
+        var message = "Cleared cache.\n" + oks + " deleted OK\n" + fails + " failed";
+        alert(message);
+        testUsage();
+    })
+}
+
